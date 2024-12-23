a60428de4f1e890a42fb9df8b9bc2fe3dc365d95
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-12-23 14:27:16 +0700

    ....

--- laravel_01/resources/js/Pages/Contractor/Pages/Home.jsx
+++ laravel_01/resources/js/Pages/Contractor/Pages/Home.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import TotalBalance from '@/Components/Contractor/TotalBalance';
-import ColChartReport from '@/Components/Contractor/ColChartReport';
-import Header from '@/Components/Contractor/Header';
+
 const Home = () => {
     return (
-        <div className="h-full">
-            <div className='w-[95%] mx-auto'>
-                <Header />
-                <TotalBalance />
-                <ColChartReport/>
-            </div>
+        <div>
+
         </div>
     );
 }
